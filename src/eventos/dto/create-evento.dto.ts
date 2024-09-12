a33d893effd574b76a292a9b43c873e1f5ed6805
samumedigo1411 <samuel.medina@eia.edu.co>
2024-09-12 00:02:44 -0500

    Update create-evento.dto.ts

--- src/eventos/dto/create-evento.dto.ts
+++ src/eventos/dto/create-evento.dto.ts
@@ -1,1 +1,23 @@
-export class CreateEventoDto {}
+import { IsString, IsNotEmpty, IsDate, IsNumber } from 'class-validator';
+
+export class CreateEventoDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsDate()
+  @IsNotEmpty()
+  fecha: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  hora: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  cover: number;
+
+  @IsString()
+  @IsNotEmpty()
+  tematica: string;
+}
